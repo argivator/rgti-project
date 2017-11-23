df2109172c7597375ee44e9a47441e75497d31f3
Domen Vidmar <dv9376@student.uni-lj.si>
2017-11-23 12:41:16 +0100

    Dodajanje metkov kazalec, trava in nebo bolse barve

--- scripts/zombie_defense.js
+++ scripts/zombie_defense.js
@@ -30,6 +30,8 @@ var bullets = [];
 var stevecMetkov = 0; // za identifikacijo dolocenega metka
 var lahkoStrelja = true;
 
+var stMetkov = 5;
+
 // wall config
 
 var walls = [];
@@ -336,7 +338,7 @@ function initTextures() {
   grassTexture.image.onload = function () {
     handleTextureLoaded(grassTexture)
   }
-  grassTexture.image.src = "./assets/grass.png";
+  grassTexture.image.src = "./assets/grass.jpg";
 
   playerTexture = gl.createTexture();
   playerTexture.image = new Image();
@@ -369,7 +371,7 @@ function initTextures() {
   bulletTexture.image.onload = function(){
     handleTextureLoaded(bulletTexture);
   }
-  bulletTexture.image.src = "./assets/bullet.png";
+  bulletTexture.image.src = "./assets/bullet.jpg";
 
 }
 
@@ -1507,10 +1509,27 @@ function handleKeys() {
   if(currentlyPressedKeys[32]){
     if(lahkoStrelja){
       addBullet();
+      stMetkov = stMetkov - 1;
+      document.getElementById("stevec").innerHTML = stMetkov;
       lahkoStrelja = false;
-      setTimeout(function(){
-        lahkoStrelja = true;
-      }, 500);
+      if(stMetkov > 0){
+        setTimeout(function(){
+          lahkoStrelja = true;
+        }, 500);
+      }else{
+        document.getElementById("stevec").innerHTML = "RELOADING!";
+        document.getElementById("metkiStevec").classList.toggle("red");
+        document.getElementById("metkiStevec").classList.toggle("yellow");
+        setTimeout(function(){
+          lahkoStrelja = true;
+          stMetkov = 5;
+          document.getElementById("stevec").innerHTML = stMetkov;
+          document.getElementById("metkiStevec").classList.toggle("red");
+          document.getElementById("metkiStevec").classList.toggle("yellow");
+        }, 1000);
+        
+      }
+      
     }
   }
 
@@ -1568,7 +1587,7 @@ function start() {
 
   // Only continue if WebGL is available and working
   if (gl) {
-    gl.clearColor(0.52, 0.81, 0.92, 1.0);                      // Set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.6, 0.6, 1.0);                      // Set clear color to black, fully opaque
     gl.clearDepth(1.0);                                     // Clear everything
     gl.enable(gl.DEPTH_TEST);                               // Enable depth testing
     gl.depthFunc(gl.LEQUAL);                                // Near things obscure far things
